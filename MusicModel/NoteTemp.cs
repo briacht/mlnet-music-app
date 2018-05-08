563bba58277363f7ba94f8c8a5171d886136b145
Rowan Miller <rowanmiller@users.noreply.github.com>
2018-05-08 01:01:10 -0700

    Move out NoteTemp

---
+++ MusicModel/NoteTemp.cs
@@ -0,0 +1,10 @@
+﻿namespace MusicModel
+{
+    public class NoteTemp
+    {
+        public int Chorale { get; set; }
+        public int Measure { get; set; }
+        public int Note { get; set; }
+        public int KeySignature { get; set; }
+    }
+}
--- MusicModel/Program.cs
+++ MusicModel/Program.cs
@@ -147,15 +147,4 @@ namespace MusicModel
             Console.WriteLine($"Predicted {result.NoteNumber}");
         }
     }
-
-    public class NoteTemp
-    {
-        public int Chorale { get; set; }
-        public int Measure { get; set; }
-        public int Note { get; set; }
-        public int KeySignature { get; set; }
-    }
-
-
-    
 }
